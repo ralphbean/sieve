0cad876b8fbd31d4d46611842b966458a5dfb460
Ralph Bean <rbean@redhat.com>
2012-06-22 12:58:38 -0400

    Py2.5 support.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def long_description():
     f.close()
     return content.split('split here', 1)[1]
 
-version = '0.1.4'
+version = '0.1.5'
 
 setup(name='sieve',
       version=version,
@@ -29,6 +29,7 @@ setup(name='sieve',
           "Topic :: Text Processing :: Markup :: HTML",
           "Topic :: Text Processing :: Markup :: XML",
           "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.5",
           "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3",
