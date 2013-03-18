9133f5b0c71670bf7bfe371eac09f7e48df6a871
Ralph Bean <rbean@redhat.com>
2013-03-18 15:57:21 -0400

    More loose with xml.

--- sieve/operators.py
+++ sieve/operators.py
@@ -9,10 +9,14 @@ import six
 
 def _xml_from_string(string):
     try:
-        return html.fromstring(string)
+        return etree.fromstring(string)
     except etree.XMLSyntaxError:
-        print("choked on: " + string)
-        raise
+        try:
+            return html.fromstring(string)
+        except etree.XMLSyntaxError:
+            print("choked on: " + string)
+            raise
+
 
 def assert_eq_xml(xml1, xml2, message=None, wrapped=False):
     if not message:
@@ -20,6 +24,7 @@ def assert_eq_xml(xml1, xml2, message=None, wrapped=False):
         reporter = lambda x: buf.write(x + '\n')
         result = eq_xml(xml1, xml2, reporter=reporter, wrapped=wrapped)
         if not result:
+            reporter("xml1 %s\n does not equal xml2\n %s" % (xml1, xml2))
             reason = buf.getvalue()
             assert result, reason
     else:
@@ -32,6 +37,7 @@ def assert_in_xml(needle, haystack, message=None, wrapped=False):
         reporter = lambda x: buf.write(x + '\n')
         result = in_xml(needle, haystack, reporter=reporter, wrapped=wrapped)
         if not result:
+            reporter("needle %s\n not in haystack\n %s" % (needle, haystack))
             reason = buf.getvalue()
             assert result, reason
     else:
@@ -56,3 +62,4 @@ def in_xml(needle, haystack, reporter=None, wrapped=False):
     tree1 = _xml_from_string(wrapper(needle))
     tree2 = _xml_from_string(wrapper(haystack))
     return c.xml_contains(tree1, tree2, reporter)
+
